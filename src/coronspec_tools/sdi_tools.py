a71ce108d1732e8d3e1817e204ee4ce9d8416246
Jonathan Aguilar <jaguilar@stsci.edu>
2025-12-01 14:51:24 -0500

    moved some psf interpolation tools into sdi_tools
    this includes how to set the mask for interpolating the PSF through a row

--- src/coronspec_tools/sdi_tools.py
+++ src/coronspec_tools/sdi_tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import ndimage
+from scipy import interpolate
 import pyklip
 
 def calc_scaling(
@@ -49,8 +50,111 @@ def rescale_img(
     row_coords, col_coords = np.mgrid[:img.shape[0], :img.shape[1]]
     row_sep = row_coords - center_row
     new_rows = (row_sep * scale_factors) + center_row
-    scaled_img = ndimage.map_coordinates(img.copy(), [new_rows, col_coords] )
+    scaled_img = ndimage.map_coordinates(img.copy(), [new_rows, col_coords], mode='nearest')
     return scaled_img
 
 
+
+def calc_wl_mask_position(
+    y0 : float,
+    y1 : int,
+	ycen : float,
+	ref_wl_ind : float,
+	psf_width : float
+) -> tuple[float, float]:
+    """
+    Compute the center and width of the mask, along the wavelength axis, required to
+    mask out a rescaled companion PSF.
+    Returns a tuple of [mask_center, mask_width].
+    Note that the mask width does NOT depend on y0, only on y1, so you can use
+    it regardless of the value of y0.
+
+    Parameters
+    ----------
+    y0 : float
+      the location of the source in the original (unscaled) image
+    y1 : int
+      the row you are interpolating
+	ycen: float
+      the central row from which the scaling is computed
+	ref_wl_ind : float
+      the index of the reference wavelength for the scaling
+	psf_width : float
+      the spatial (y) half-size of the psf
+
+    Output
+    ------
+    mask_width : float
+      the mask-width in wavelength space required to mask out the spatially-rescaled PSF
+
+    """
+    width = 2 * psf_width * ref_wl_ind / (y1 - ycen)
+    center = ref_wl_ind * (y0-ycen) / (y1-ycen)
+    return center, width
+
+def compute_wl_mask_center(
+    y : int,
+	y0 : float,
+	ref_wl_ind : float,
+	psf_width : float
+) -> float:
+    """
+    Compute the width of the mask, along the wavelength axis, required to
+    mask out a rescaled companion PSF.
+
+    Parameters
+    ----------
+    y : int
+      the row you are interpolating
+	y0 : float
+      the central row from which the scaling is computed
+	ref_wl_ind : float
+      the index of the reference wavelength for the scaling
+	psf_width : float
+      the spatial (y) half-size of the psf
+
+    Output
+    ------
+    mask_width : float
+      the mask-width in wavelength space required to mask out the spatially-rescaled PSF
+
+    """
+    width = 2 * psf_width * ref_wl_ind / (y - y0)
+    return width
  
+def interpolate_by_row(
+        img : np.ndarray,
+        mask_width : int = 3,
+) -> np.ndarray :
+    """
+    Interpolate an image row-by-row. For each row, mask a region off and then predict it by interpolation.
+
+    Parameters
+    ----------
+    img : np.ndarray
+      2-D wavelength-scaled spectral image (speckles move across rows)
+    mask_width : int = 3
+      how many pixels before and after the test pixel to mask
+
+    Output
+    ------
+    interp_img : np.ndarray
+      the image as replaced by interpolated values
+    """
+    interp_img = np.empty_like(img)
+
+    for col in np.arange(mask_width, img.shape[1]-mask_width):
+        new_mask = np.zeros_like(img, dtype=bool)
+        lb, ub = col-mask_width, col+mask_width+1
+        lb = max([ 0, lb ])
+        ub = min([ img.shape[1], ub ])
+        new_mask[:, lb : ub] = True
+        masked_img = np.stack([row[~row_mask] for row, row_mask in zip(img, new_mask)])
+        # interp along the column axis
+        interp_row = interpolate.CubicSpline(
+            np.arange(img.shape[1])[~new_mask[0]],
+            masked_img, 
+            axis=1
+        )
+        interp_img[:, col] = interp_row(col)
+    return interp_img
