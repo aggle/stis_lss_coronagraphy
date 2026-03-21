bbf735680733ee2e76f0b59897ee05e3933a2910
Jonathan Aguilar <jaguilar@stsci.edu>
2026-03-20 23:46:44 -0400

    added PDF model and forward-modeled injection signal to plotting

--- src/coronspec_tools/diagnostic_plots.py
+++ src/coronspec_tools/diagnostic_plots.py
@@ -17,9 +17,16 @@ from coronspec_tools import (
 def plot_injection_results(
         ret: retrieval_tools.Retriever,
         row : pd.Series
-):
+) -> mpl.figure.Figure:
     """
     Plot the results of injecting and retrieving a spectrum
+
+    Parameters
+    ----------
+    ret: retrieval_tools.Retriever
+      a Retriever instance
+    row : pd.Series
+      a row of ret.inj_results e.g. ret.inj_results.loc[21]
     """
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
 
@@ -46,9 +53,13 @@ def plot_injection_results(
         ax.plot(row['trace'], ls='--', c='gray')
 
     ax = axes[1, 1]
-    ax.set_title("Recovered signal")
-    ax.plot(ret.inj_trace.max(axis=0), label='injected')
-    ax.plot(row['signal'], label='recovered')
+    ax.set_title("Recovered signal [single row]")
+    trace_center = np.floor(ret.inj_trace.shape[0]/2).astype(int)
+    inj_spec = ret.inj_trace[trace_center]
+    ax.plot(inj_spec, label='injected', c='C0')
+    ax.plot(row['signal'], label='recovered', c='C1')
+    ax.plot(row['model_descaled'], label='psf model', c='C2')
+    ax.plot(row['fm_injection'], label='FM injection', c='C3')
     ax.legend()
 
     return fig
@@ -65,12 +76,13 @@ def plot_row_fitting(
     nrows = np.ceil(naxes/ncols).astype(int)
     fig, axes = plt.subplots(
         nrows=nrows, ncols=ncols, figsize=(5*ncols, 5*nrows),
-        sharex=True, sharey=True
+        sharex=True, sharey=False,
     )
 
     cols = np.arange(residual_img.shape[1])
 
-    for i, (ax, scaled_row_ind) in enumerate(zip(axes.flat, scaled_ind)):
+    for i, (ax, scaled_row_ind) in enumerate(zip(axes.flat, scaled_ind[::-1])):
+        i = scaled_ind.size - 1 - i
         ax.set_title(f"Scaled row: {scaled_row_ind}")
         mask = results_row['stamp_mask'][i]
         scaled_row = results_row['scaled_stamp'][i]
