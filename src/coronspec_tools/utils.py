963f09c20428be1fec8d94c5241ce8afed007229
Jonathan Aguilar <jaguilar@stsci.edu>
2026-03-31 09:58:06 -0400

    trying different data cleaning functions

--- src/coronspec_tools/utils.py
+++ src/coronspec_tools/utils.py
@@ -185,6 +185,7 @@ def make_sx2_mask(
 
 
 def rolling_median(array, window=1):
+    # replace each pixel with the median of its four neighbors
     median_filtered = np.zeros(array.size)
     for i in np.arange(array.size):
         lb, ub = i-window, i+window+1
@@ -193,10 +194,25 @@ def rolling_median(array, window=1):
         median_filtered[i] = np.nanmedian(array[lb:ub])
     return median_filtered
 
+def median_neighbor_replace(array):
+    """
+    Median replace each pixel in a 1-D array with the median of its 4 neighbors
+    """
+    window = 2
+    clean_a = np.zeros_like(array)*np.nan
+    for i in range(len(array)):
+        lb, ub = i-window, i+window+1        
+        lb = max([ lb, 0 ]) + 1
+        ub = min([ ub, array.size ])
+        subset = np.concat((array[lb:i], array[lb+1:ub]))
+        clean_a[i] = np.nanmedian(subset)
+    return clean_a
+
 def median_filter_image(img, window=5):
     filtered_img = np.zeros_like(img) * np.nan
     for row in np.arange(filtered_img.shape[0]):
-        filtered_img[row] = rolling_median(img[row], window)
+        # filtered_img[row] = rolling_median(img[row], window)
+        filtered_img[row] = median_neighbor_replace(img[row])
     return filtered_img
 
 
