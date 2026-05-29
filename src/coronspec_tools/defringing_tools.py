ebfd1b328adfb5a230400e80675dcce9e68a723f
Jonathan Aguilar <jaguilar@stsci.edu>
2026-05-29 16:49:34 -0400

    some defringing tools that don't work well yet

--- src/coronspec_tools/defringing_tools.py
+++ src/coronspec_tools/defringing_tools.py
@@ -14,9 +14,11 @@ import stistools
 from IPython.utils import io
 
 def defringe_raw2d(
-    sci_file : str | Path,
-    flat_file : str | Path,
-    wavecal_file : str | Path,
+    crj_file : str | Path,
+    # flat_file : str | Path,
+    # wavecal_file : str | Path,
+    # sci_id : str,
+    # data_folder : str | Path = '.',
     output_dir : str | Path = '.',
     beg_shift=-0.5,
 	end_shift=2,
@@ -25,6 +27,7 @@ def defringe_raw2d(
 	end_scale=1.7,
 	scale_step=0.04,
     compare_spectra : bool = False,
+    extraction_row : int | None = None,
 ):
     """
     Apply fringe flat correction to 2-D spectral images
@@ -33,16 +36,31 @@ def defringe_raw2d(
     ----------
     crj_file : str | Path
       2-D spectral images with fringes that need correcting
-    flat_file : str | Path
-      2-D fringe flat image to use for correcting fringes
+    output_dir : str | Path = '.'
+      the directory for writing the generated files
+    beg_shift=-0.5
+	end_shift=2
+	shift_step=0.1
+    beg_scale=0.8
+	end_scale=1.7
+	scale_step=0.04
+    compare_spectra : bool = False
     """
-    mode = fits.getval(sci_file,'OPT_ELEM', 0)
-    aper = fits.getval(sci_file,'APERTURE', 0)
+    # data_folder = Path(data_folder)
+    crj_file = Path(crj_file)
+    rootname = fits.getval(crj_file, 'ROOTNAME', 0).lower()
+    data_folder = crj_file.parent
+
+    mode = fits.getval(crj_file,'OPT_ELEM', 0)
+    aper = fits.getval(crj_file,'APERTURE', 0)
+
     # Normalize the fringe flat
-    sci_file = Path(sci_file)
-    flat_file = Path(flat_file)
-    wavecal_file = Path(wavecal_file)
+    flat_id = fits.getval(crj_file, 'FRNGFLAT', 0).lower()
+    flat_file = data_folder / f"{flat_id}_raw.fits"
+
     output_dir = Path(output_dir)
+    normflat_file =  output_dir /f"{flat_id}_nsp.fits"
+    wavecal_file = output_dir / f"{rootname}_wav.fits"
     normflat_file = output_dir / (flat_file.name.replace("raw","nsp"))
     stistools.defringe.normspflat(
         str(flat_file),
@@ -54,11 +72,6 @@ def defringe_raw2d(
     if mode == 'G750L':
         with fits.open(normflat_file, mode='update') as hdulist:
             hdulist[1].data[:,:250] = 1
-
-    # Run prepspec to make a crj file
-    with io.capture_output() as captured:
-        stistools.defringe.prepspec(str(sci_file), outroot=str(output_dir))
-    crj_file = output_dir / sci_file.name.replace("raw","crj")
 
     # find the optimal scaling to make the fringe flat
     frr_file = Path(str(normflat_file).replace("nsp","frr"))
@@ -78,16 +91,21 @@ def defringe_raw2d(
     drj_file = stistools.defringe.defringe(
         str(crj_file), str(frr_file), overwrite=True
     )
+
     if aper == '52X0.2':
         infile = Path(drj_file)
-        output_path = Path(".") /  infile.name.replace(infile.stem.split("_")[-1], 'dx1d')
-        drj_specfile = extract_spectrum(drj_file, str(output_path))
+        output_fname = output_dir /  infile.name.replace(infile.stem.split("_")[-1], 'dx1d')
+        if output_fname.exists():
+            os.remove(output_fname)
+            print(output_fname, output_fname.exists())
+        drj_specfile = extract_spectrum_from_2d(drj_file, str(output_fname), extraction_row)
 
         if compare_spectra:
             infile = Path(crj_file)
-            output_path = Path(".") /  infile.name.replace(infile.stem.split("_")[-1], 'x1d')
-            crj_specfile = extract_spectrum(crj_file, str(output_path))
-
+            output_fname = output_dir /  infile.name.replace(infile.stem.split("_")[-1], 'x1d')
+            if output_fname.exists():
+                os.remove(output_fname)
+            crj_specfile = extract_spectrum_from_2d(crj_file, str(output_fname), extraction_row)
             # Plot both the fringed and the defringed 1D extracted spectra together
             dx1d = fits.open(drj_specfile)
             x1d = fits.open(crj_specfile)
@@ -112,17 +130,17 @@ def defringe_raw2d(
     return drj_file
 
 
-def extract_spectrum(
+def extract_spectrum_from_2d(
         file2d : str | Path,
-        output_dir : str | Path,
+        output_name : str | Path | None = None,
+        extraction_row : int | None = None,
 ):
     """
+    file2d: 2d spectral file
+    output_name : full path to output 1d spectrum file
     """
     file2d = Path(file2d)
-    output_path = Path(output_dir)
-    # output_dir = Path(output_dir)
-    # ftype = file2d.stem.split("_")[-1]
-    # output_path = output_dir /  file2d.name.replace(ftype, 'x1d')
+    output_name = Path(output_name)
     with io.capture_output() as captured:
-        stistools.x1d.x1d(str(file2d), output=str(output_path))
-    return output_path
+        stistools.x1d.x1d(str(file2d), output=str(output_name), a2center=extraction_row)
+    return output_name
